4eb762ca20821b8c5d088bdcf7e7e609ac3216d6
Kanaciel <10223030@student.vgu.edu.vn>
2025-12-14 11:04:46 +0100

    send help

--- CNN/model.py
+++ CNN/model.py
@@ -1,2 +1,13 @@
 import torch
 import torch.nn as nn
+
+
+
+class Classifier(nn.Module):
+      def __init__(self):
+            super().__init__()
+            #none distinct stuffs
+            self.flatten = nn.Flatten()
+            self.relu = nn.ReLU()
+            
+            #distinct stuffs
